b0f78eaa03260307ae4144b8952a65391a3e1da2
hoaneeee <hoavipyahoo@gmail.com>
2025-11-20 09:22:47 +0700

    update 6

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Stars, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { ShaderMaterial } from "three";
 import { GradientTexture } from "@react-three/drei";
-import teacherImg from "../assets/teacher.jpg";
+import teacherImg from "../assets/chi.jpg";
 
 
 // ----- CUSTOM FRESNEL SHADER -----
